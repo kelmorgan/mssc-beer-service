6b3656bf6c88dee1bd7f00ca62d710e8e36d049f
kelmorgan <kelmorgan18@gmail.com>
2022-05-27 12:48:08 +0100

    Adding validation to BeerDto

--- src/main/java/com/kelmorgan/msscbeerservice/api/v1/models/BeerDto.java
+++ src/main/java/com/kelmorgan/msscbeerservice/api/v1/models/BeerDto.java
@@ -3,6 +3,10 @@ package com.kelmorgan.msscbeerservice.api.v1.models;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -11,14 +15,26 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @NotBlank
     private String beerName;
+    @NotBlank
     private BeerStyle beerStyle;
+
     private Integer quantityOnHand;
+    @NotNull
+    @Positive
     private BigDecimal price;
+
+    @NotNull
+    @Positive
     private Long upc;
 
 }
