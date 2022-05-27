0bb4535f2420f03c1138ee49ba8884041fbb3a00
kelmorgan <kelmorgan18@gmail.com>
2022-05-27 10:03:16 +0100

    created entity

---
+++ src/main/java/com/kelmorgan/msscbeerservice/domain/Beer.java
@@ -0,0 +1,48 @@
+package com.kelmorgan.msscbeerservice.domain;
+
+import com.kelmorgan.msscbeerservice.api.v1.models.BeerStyle;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+
+    @Enumerated(EnumType.STRING)
+    private BeerStyle beerStyle;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+    private BigDecimal price;
+
+    @Column(unique = true)
+    private Long upc;
+}
---
+++ src/main/java/com/kelmorgan/msscbeerservice/domain/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.kelmorgan.msscbeerservice.domain.repositories;
+
+import com.kelmorgan.msscbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
